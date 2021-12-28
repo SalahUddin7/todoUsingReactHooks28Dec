dab48ddcbd61b5436b7143b2a664fd6a2d1a5918
Salah Uddin <ziko.salahuddin@gmail.com>
2021-12-28 14:26:53 +0100

    To Do Form Setting up 23.35

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React from 'react';
 import FormInput from './components/FormInput';
 import List from './components/List';
 import Footer from './Footer';
+import { DataProvider } from './components/DataProvider';
 
 function App() {
   return (
-    <div className="App">
-      <h1>To Do List</h1>
+    <DataProvider>
+      <div className="App">
+        <h1>To Do List</h1>
 
-      <FormInput />
-      <List />
-      <Footer />
-    </div>
+        <FormInput />
+        <List />
+        <Footer />
+      </div>
+    </DataProvider>
   );
 }
 
